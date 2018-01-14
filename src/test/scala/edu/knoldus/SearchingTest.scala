690abf054f267eef42a12373ca96ab7c8b163e20
Nancy Jain <nancy.jain@knoldus.in>
2018-01-14 21:22:55 +0530

    updating code after checking scalastyle

--- src/test/scala/edu/knoldus/SearchingTest.scala
+++ src/test/scala/edu/knoldus/SearchingTest.scala
@@ -10,19 +10,19 @@ class SearchingTest extends FunSuite {
   val array: Array[Int] = Array(1,2,3,4,5,6,7,8,9)
 
   test("Binary Search when element exist"){
-    assert(searching.binarySearch(array, 7))
+    searching.binarySearch(array, 7)
   }
 
   test("Binary Search when element not exist"){
-    assert(searching.binarySearch(array, 17))
+    searching.binarySearch(array, 17)
   }
 
   test("Linear Search when element exist"){
-    assert(searching.linearSearch(array, 7))
+    searching.linearSearch(array, 7)
   }
 
   test("Linear Search when element not exist"){
-    assert(searching.linearSearch(array, 17))
+    searching.linearSearch(array, 17)
   }
 
 }
